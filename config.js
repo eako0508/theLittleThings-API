4ff1315bbd292b52a9a88bba733f9f5546d8903c
Eugene <eako0508@gmail.com>
2018-04-09 10:33:03 -0400

    added config file

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.DATABASE_URL = process.env.DATABASE_URL
+	|| 'mongodb://admin:admin@ds239439.mlab.com:39439/thelittlethings';
+exports.TEST_DATABASE_URL = 'mongodb://admin:admin@ds157487.mlab.com:57487/thelittlethings-test';
